67fd7c310d1830fe926c1067b6c0cc27be565e80
Yash2108 <1999yashjain@gmail.com>
2021-05-20 15:35:08 +0530

    Implemented basic messaging. Implemented individual user initialization. Adding Airline Data.
    Co-authored-by: praanavgupta@gmail.com <praanavgupta@gmail.com>

---
+++ starter.py
@@ -0,0 +1,44 @@
+import discord
+import json
+
+data={
+    'Flight1':{
+        'Customer Satisfaction':0,
+        'Overall Rating':0,
+        'Value For Money':0
+    }
+}
+
+
+client= discord.Client()
+
+@client.event
+async def on_ready():
+    print("Ready")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('.hello'):
+        await message.channel.send('Hello World!')
+
+    if message.content.startswith('.start'):
+        with open('user_data.json') as json_file:
+            data = json.load(json_file)
+            print(data.keys(), type(message.author.id))
+            if str(message.author.id) in data.keys():
+                print('already exists')
+                await message.channel.send('User already initialized')
+            else:
+                post_data={
+                    message.author.id:'Initialized'
+                }
+                data.update(post_data)
+                with open('user_data.json', 'w') as outfile:
+                    json.dump(data, outfile)
+                    await message.channel.send('initialized')
+
+                    
+
+client.run('ODQ0ODY1NDEzMjI5MDUxOTA0.YKYoOw.bPgQ6LMs5nYD1DqHUQVBx9AAIXs')
