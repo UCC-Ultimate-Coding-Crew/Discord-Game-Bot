983cdddfe7496a1208e66f369d58e6aec16ffcd0
Pranav Gupta <pranavgupta2305@gmail.com>
2021-06-04 13:30:56 +0530

    Very Minor changes

--- help.py
+++ help.py
@@ -4,13 +4,12 @@ async def helpout(message):
     await message.channel.send("Welcome to the Airline Trump Cards")
     await message.channel.send("Here a list of commands to play around with:")
     
-    statements=[".start assigns you 2 random cards in the start",".battle gets you into the battleground to face an opponent"]
+    statements=["   .help gets you the help menu!","   .start assigns you 2 random cards in the start","    .battle gets you into the battleground to face an opponent"]
     printing="```"
     for i in statements:
         printing+="\n"
         printing+=i
-        print('yolo', printing)
+    
 
-    printing="```"
+    printing+="```"
     await message.channel.send(printing)
-    print(printing)
