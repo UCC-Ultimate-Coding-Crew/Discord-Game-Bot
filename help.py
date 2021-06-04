0e344d512efae5861845587a06f423c940d91134
Pranav Gupta <pranavgupta2305@gmail.com>
2021-06-04 13:22:21 +0530

    Minor Changes

--- help.py
+++ help.py
@@ -1,4 +1,15 @@
-print("Welcome to the Airline Trump Cards")
-print("Here a list of commands to play around with:")
 
-list statements=[".start assigns you 2 random cards in the start",".battle gets you into the battleground to face an opponent"]
+async def helpout():
+    print("Welcome to the Airline Trump Cards")
+    print("Here a list of commands to play around with:")
+
+    statements=["   .help gets you the help menu!","   .start assigns you 2 random cards in the start","   .battle gets you into the battleground to face an opponent"]
+
+    printing="```"
+    for i in statements:
+        printing+="\n"
+        printing+=i
+        
+
+    printing="```"
+    print(printing)
