b32f1b1f8e7cf243f0780386d582561e50d52c1b
Yash2108 <1999yashjain@gmail.com>
2021-06-04 14:37:59 +0530

    Working on battle command.

--- battle.py
+++ battle.py
@@ -1,8 +1,17 @@
 import discord
 import json
 
-async def battle2v2(message):
+async def battle2v2(message, client):
     opponent=message.mentions[0]
-    # opponent = discord.User(opponent)
     print(message.mentions)
     await message.channel.send('{} has requested a battle with {}.\n Do you accept?[Y/n]'.format(message.author.mention, opponent.mention))
+    
+    def check(m):
+        return m.author == opponent and m.channel == message.channel
+    accept=['yes', 'y']
+    msg = await client.wait_for("message", check=check)
+    if str(msg.content).lower() in accept:
+        await msg.channel.send("Accepted")
+    else:
+        await msg.channel.send("Battle Denied")
+    
--- starter.py
+++ starter.py
@@ -6,14 +6,6 @@ from battle import battle2v2
 from help import helpout
 from trade import cardstrade
 
-data={
-    'Flight1':{
-        'Customer Satisfaction':0,
-        'Overall Rating':0,
-        'Value For Money':0
-    }
-}
-
 client= discord.Client()
 
 @client.event
@@ -27,7 +19,7 @@ async def on_message(message):
     if message.content.startswith('.start'):
         await create_new_user(message)
     if message.content.startswith('.battle'):
-        await battle2v2(message)
+        await battle2v2(message, client)
     if message.content == '.help':
         await helpout(message)
     if message.content == '.trade':
