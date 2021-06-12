eb30b84f694f2fd629fb8575348dc91db3825585
Yash2108 <1999yashjain@gmail.com>
2021-06-13 02:45:30 +0530

    Finished basic battle procedures.

--- battle.py
+++ battle.py
@@ -1,23 +1,22 @@
 import discord
 import json
 from random import choice, sample
-
-async def getCards(message):
-    await message.channel.send("{}\nYour Cards:".format(message.author.mention))
-    with open('user_data.json') as json_file:
-            data = json.load(json_file)
-            if str(message.author.id) in data.keys():
-                for i in data[str(message.author.id)]['Cards']:
-                    j= data[str(message.author.id)]['Cards'][i]
-                    with open('cards.json') as card:
-                        airline = json.load(card)
-                        usercards=airline[j]["Airline Name"]
-                        await message.channel.send(usercards)
+from utilities import getCardsWithStats, getCards, printCards, printTopCard
+# async def getCards(message):
+#     await message.channel.send("{}\nYour Cards:".format(message.author.mention))
+#     with open('user_data.json') as json_file:
+#             data = json.load(json_file)
+#             if str(message.author.id) in data.keys():
+#                 for i in data[str(message.author.id)]['Cards']:
+#                     j= data[str(message.author.id)]['Cards'][i]
+#                     with open('cards.json') as card:
+#                         airline = json.load(card)
+#                         usercards=airline[j]["Airline Name"]
+#                         await message.channel.send(usercards)
 
 async def battle2v2(message, client):
     opponent=message.mentions[0]
     await message.channel.send('{} has requested a battle with {}.\n Do you accept?[Y/n]'.format(message.author.mention, opponent.mention))
-    
     def check(m):
         return m.author == opponent and m.channel == message.channel
     
@@ -27,10 +26,47 @@ async def battle2v2(message, client):
         await message_confirm.channel.send("Battle Denied")
         return
     await message_confirm.channel.send("Accepted")
-    await getCards(message_confirm)
-    await getCards(message)
-    current_player=choice([message.author, opponent])
-    # p1_cards=sample(test_list, len(test_list))
-    # p2_cards
-    # while True:
+    await printCards(message_confirm)
+    await printCards(message)
+
+    p1=choice([message.author, opponent])
+    p1_cards=getCardsWithStats(message.author.id)
+    p1_ShuffledCards=sample(p1_cards, len(p1_cards))
+    p2=opponent if p1==message.author else message.author
+    p2_cards=getCardsWithStats(message_confirm.author.id)
+    p2_ShuffledCards=sample(p2_cards, len(p2_cards))
+
+    await message.channel.send("{} will start.".format(p1.mention))
+    while True:
+        await message.channel.send("{}, your top card is:".format(p1.mention))
+        await printTopCard(message.channel,p1.id, p1_ShuffledCards)
+        await message.channel.send("Choose an attribute.")
+        def check_opponent(m):
+            return m.channel == message.channel
+        attribute = await client.wait_for("message", check=check_opponent)
+        if p1_ShuffledCards[0][attribute.content] > p2_ShuffledCards[0][attribute.content]:
+            await message.channel.send("{} won this round.".format(p1.mention))
+            await message.channel.send("{} had:".format(p2.mention))
+            await printTopCard(message.channel, p2.id, p2_ShuffledCards)
+            
+            p1_ShuffledCards.append(p1_ShuffledCards.pop(0))
+            p1_ShuffledCards.append(p2_ShuffledCards.pop(0))
+
+            if len(p2_ShuffledCards)==0:
+                await message.channel.send("{} has won!".format(p1.mention))
+                break
+        else:
+            await message.channel.send("{} won this round.".format(p2.mention))
+            await message.channel.send("{} had:".format(p1.mention))
+            await printTopCard(message.channel,p1.id, p1_ShuffledCards)
+            p2_ShuffledCards.append(p2_ShuffledCards.pop(0))
+            p2_ShuffledCards.append(p1_ShuffledCards.pop(0))
+
+            if len(p1_ShuffledCards)==0:
+                await message.channel.send("{} has won!".format(p2.mention))
+                break
+            p2, p1 = p1, p2
+            p1_ShuffledCards, p2_ShuffledCards = p2_ShuffledCards, p1_ShuffledCards
+            p1_cards, p2_cards=p2_cards, p1_cards
+            
         
--- utilities.py
+++ utilities.py
@@ -2,19 +2,41 @@ import discord
 import json
 
 def getCards(uid):
-    with open('user_data.json') as json_file:
-            data = json.load(json_file)
-            if str(uid) in data.keys():
-                ls=[]
-                for i in data[str(uid)]['Cards']:
-                    j= data[str(uid)]['Cards'][i]
-                    with open('cards.json') as card:
-                        airline = json.load(card)
-                        usercards=airline[j]["Airline Name"]
-                        ls.append(usercards)
-                return ls
+	with open('user_data.json') as json_file:
+			data = json.load(json_file)
+			if str(uid) in data.keys():
+				ls=[]
+				for i in data[str(uid)]['Cards']:
+					j= data[str(uid)]['Cards'][i]
+					with open('cards.json') as card:
+						airline = json.load(card)
+						usercards=airline[j]["Airline Name"]
+						ls.append(usercards)
+				return ls
+
+def getCardsWithStats(uid):
+	with open('user_data.json') as json_file:
+			data = json.load(json_file)
+			if str(uid) in data.keys():
+				ls=[]
+				for i in data[str(uid)]['Cards']:
+					j= data[str(uid)]['Cards'][i]
+					with open('cards.json') as card:
+						airline = json.load(card)
+						usercard=airline[j]
+						ls.append(usercard)
+				return ls
 
 async def printCards(message):
-    await message.channel.send("{0} Your Cards:".format(message.author.mention))
-    for i in getCards(message.author.id):
-        await message.channel.send(i)
+	await message.channel.send("{0} Your Cards:".format(message.author.mention))
+	for i in getCards(message.author.id):
+		await message.channel.send(i)
+
+async def printTopCard(channel, uid, shuffled=None):
+	if shuffled == None:
+		shuffled=getCardsWithStats(uid)
+	txt='```'
+	for j in shuffled[0].items():
+		txt+=str(j[0])+":\t"+str(j[1])+"\n"
+	txt+='```'
+	await channel.send(txt)
