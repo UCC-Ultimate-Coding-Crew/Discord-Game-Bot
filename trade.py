26a3404af888fbda291ba88eb077c6d7689153f1
Pranav Gupta <pranavgupta2305@gmail.com>
2021-06-15 12:52:51 +0530

    Completed Trade

--- trade.py
+++ trade.py
@@ -1,27 +1,48 @@
 import discord
+from utilities import getTotalCards, printCardCodes, completeTrade
 import json
-from utilities import getCards, printCards
 
 async def cardstrade(message, client):
 	await message.channel.send('{0} wants to trade'.format(message.author.mention))
 	await message.channel.send("Anyone wanting to trade these cards, .accept is the command")
 	accept=['.accept']
+
 	def check(m):
 		return m.channel == message.channel and message.author != m.author
-	def checkChannel(m):
-		return m.channel == message.channel
-	msg = await client.wait_for("message", check=check)
+	def checkOne(m):
+		return m.channel == message.channel and message.author == m.author
+	def checkTwo(m):
+		return m.channel == message.channel and msg.author == m.author
+	msg = await client.wait_for("message",check=check) 
 	if str(msg.content).lower() in accept:
 		await message.channel.send('{0} has accepted the trade'.format(msg.author.mention))
-		await printCards(message)
-		await message.channel.send('{0} which card do you wish to trade'.format(message.author.mention))
-		cardOne=await client.wait_for("message",check=checkChannel)
-		if (check_validity(cardOne)):
-			await printCards(msg)
-			await message.channel.send('{0} which card do you wish to trade'.format(msg.author.mention))
-			cardTwo=await client.wait_for("message",check=checkChannel)
-			if(check_validity(cardTwo)):
-				await message.channel.send('{0} send .accept to accept the trade'.format(message.author.mention))
+		await printCardCodes(message)
+		await message.channel.send('{0} enter the card number on the left of the card you wish to trade'.format(message.author.mention))
+		while True:
+			cardOne = await client.wait_for(("message"), check=checkOne) 
+			if (check_validity(cardOne)):
+				break
+			else:
+				await message.channel.send('Please enter a valid card number')
+		await printCardCodes(msg)
+		await message.channel.send('{0} enter the card number on the left of the card you wish to trade'.format(msg.author.mention))
+		while True:
+			cardTwo = await client.wait_for(("message"), check=checkTwo) 
+			if (check_validity(cardTwo)):
+				break
+			else:
+				await message.channel.send('Please enter a valid card number')
+		await message.channel.send('{0} send .accept to accept the trade'.format(message.author.mention))
+		input = await client.wait_for("message",check=checkOne)
+		if str(input.content).lower() in accept:
+			completeTrade(cardOne,cardTwo)
+			await message.channel.send("Trade completed!")
+			await printCardCodes(message)
+			await printCardCodes(msg)
+
+
 def check_validity(message):
-	usercards=getCards(message.author.id)
-	return message.content in usercards
+	count= getTotalCards(message)
+	input=int(message.content)
+	if(input<count):
+		return True
--- utilities.py
+++ utilities.py
@@ -27,10 +27,43 @@ def getCardsWithStats(uid):
 						ls.append(usercard)
 				return ls
 
+def getAirlineCode(input):
+	with open('user_data.json') as json_file:
+		data=json.load(json_file)
+		return data[str(input.author.id)]['Cards'][input.content]
+
+
+
 async def printCards(message):
 	await message.channel.send("{0} Your Cards:".format(message.author.mention))
 	for i in getCards(message.author.id):
 		await message.channel.send(i)
+
+
+def getCardCodes(message):
+	with open('user_data.json') as json_file:
+		data = json.load(json_file)
+		cards=data[str(message.author.id)]["Cards"]
+		# ls=[]
+		# for i in data[str(message.author.id)]["Cards"]:
+		# 	ls.append(data[str(message.author.id)]["Cards"][i])
+		# 	return ls
+		return cards
+
+async def printCardCodes(message):
+	await message.channel.send("{0} Your Cards:".format(message.author.mention))
+	cards=getCardCodes(message)
+	pairs=cards.items()
+
+	for key,value in pairs:
+		name=getAirlineName(value)
+		await message.channel.send('{0} : {1}'.format(key,name))
+
+def getAirlineName(value):
+	with open('cards.json') as card:
+		airline = json.load(card)
+		return airline[value]["Airline Name"]
+
 
 async def printTopCard(channel, uid, shuffled=None):
 	if shuffled == None:
@@ -40,3 +73,28 @@ async def printTopCard(channel, uid, shuffled=None):
 		txt+=str(j[0])+":\t"+str(j[1])+"\n"
 	txt+='```'
 	await channel.send(txt)
+
+def getTotalCards(message):
+	count=0
+	with open('user_data.json') as json_file:
+		data = json.load(json_file)
+		for i in data[str(message.author.id)]['Cards']:
+			count+=1
+		return count
+
+def completeTrade(cardOne,cardTwo):
+	codeOne=getAirlineCode(cardOne) #The card the 2nd person will get
+	codeTwo=getAirlineCode(cardTwo)
+	datafile=open('user_data.json','r')
+	file=json.load(datafile)
+	file[str(cardOne.author.id)]['Cards'].pop(cardOne.content)
+	toAdd={cardOne.content:codeTwo}
+	file[str(cardOne.author.id)]['Cards'].update(toAdd)
+
+
+	file[str(cardTwo.author.id)]['Cards'].pop(cardTwo.content)
+	toAdd={cardTwo.content:codeOne}
+	file[str(cardTwo.author.id)]['Cards'].update(toAdd)
+	output=open('user_data.json', 'w')
+	json.dump(file, output)
+	
